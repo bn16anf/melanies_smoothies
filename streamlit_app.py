c54d9868f908adf14fbcc9917868c9da57b45575
bn16anf <bn16anf@gmail.com>
2025-01-31 11:47:46 +0000

    Update streamlit_app.py
    comment out st.stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # Convert Snowpark dataframe to Pandas so can use the LOC function
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop
+#st.dataframe(pd_df)
+#st.stop
 
 name_on_order = st.text_input("Name on smoothie:")
 st.write("The name on your smoothie will be: ", name_on_order)
